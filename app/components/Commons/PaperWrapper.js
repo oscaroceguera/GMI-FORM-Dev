d03fcabc8926d87d868442a0702e6fcf04c0479a
oscaroceguerabibriesca <oscaroceguera@gmail.com>
2017-01-26 17:59:21 -0700

    Finaly project

--- app/components/Commons/PaperWrapper.js
+++ app/components/Commons/PaperWrapper.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Paper } from 'material-ui'
-import { paperContainer } from './styles.css'
+import { paperContainer, logoContainer } from './styles.css'
+
+import Logo from './logo.svg'
 
 function PaperWrapper ({ children }) {
   return (
     <Paper className={paperContainer} zDepth={2}>
+      <div className={logoContainer}><img src={Logo} /></div>
       {children}
     </Paper>
 
